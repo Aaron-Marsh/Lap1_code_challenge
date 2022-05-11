db01459225130875b8cfff92411086b4c4137772
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-05-11 10:34:56 +0100

    Added dogs array and routes

--- server/server.js
+++ server/server.js
@@ -8,5 +8,60 @@ const cors = require('cors');
 server.use(cors());
 
 
+// Arrays of results
+
+let dogs = [
+    'Golden Retriever',
+    'German Shepard',
+    'Poodle',
+    'Bulldog',
+    'Pug',
+    'Siberian Husky',
+    'Border Collie',
+    'Yorkshire Terrier',
+    'Great Dane',
+    'Shih Tzu'
+]
+
+// Routes for results
+
 server.get('/', (req, res) => res.send('hello there'));
+
+// All dogs
+server.get('/dogs', (req, res) => res.send(dogs));
+
+function randomId() {
+    return Math.floor(Math.random() * 10);
+}
+
+// Random dog
+server.get('/dogs/random', (req, res) => {
+  try {
+    const dogId = randomId();
+    const dog = dogs[dogId]
+    if (!dog) {
+      throw new Error('This dog does not exist')
+    } else {
+      res.send(dog)
+    }
+  } catch (err) {
+    res.status(404).send({ message: err.message })
+  }
+})
+
+// Specific dog
+server.get('/dogs/:id', (req, res) => {
+    try {
+      const dogId = parseInt(req.params.id)
+      const dog = dogs[dogId - 1]
+      if (!dog) {
+        throw new Error('This dog does not exist')
+      } else {
+        res.send(dog)
+      }
+    } catch (err) {
+      res.status(404).send({ message: err.message })
+    }
+  })
+
 
