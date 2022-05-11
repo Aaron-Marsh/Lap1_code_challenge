8d2cd73d3d8c9e2343344b70c6b4ea28b9de2ba0
Sam Hardiment <samuelghardiment@gmail.com>
2022-05-11 16:02:56 +0100

    new html

--- client/assets/script.js
+++ client/assets/script.js
@@ -1,49 +1,51 @@
-const form = document.querySelector('form');
-let searchresult = document.getElementById('searchresult');
-let logocontainer = document.getElementsByClassName('logocontainer');
+const form = document.querySelector("form");
+let searchresult = document.getElementById("searchresult");
+let logocontainer = document.getElementsByClassName("logocontainer");
 
 const input = document.querySelector('input[type="search"]');
-const googlesearch = document.getElementById('buttonS');
-const feelinglucky = document.getElementById('buttonL')
+const googlesearch = document.getElementById("buttonS");
+const feelinglucky = document.getElementById("buttonL");
 
 function noResult() {
-    searchresult.textContent = 'Not a result. Try searching for dogs';
+  searchresult.textContent = "Not a result. Try searching for dogs";
 }
-console.log('hello test')
+console.log("hello test");
 
-googlesearch.addEventListener('click', (e) => {
-    e.preventDefault()
-  
-    
+googlesearch.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    if (input.value === 'dogs' || 'pasta') {
-        form.style.display = "none";
-        // logocontainer.setAttribute("style", "display: none;");
-        fetch(`http://localhost:3000/${input.value}`)
-        .then(resp => resp.text())
-        .then(dog => {
-            searchresult.textContent = dog;
-        })
-    } 
-    else {
-        noResult();
-    }
+  if (input.value === "dogs" || "pasta") {
+    fetch(`http://localhost:3000/${input.value}`)
+      .then((resp) => resp.text())
+      .then((data) => {
+        // changeSite();
+        searchresult.textContent = data;
+      });
+  } else {
+    noResult();
+  }
+});
 
-})
+feelinglucky.addEventListener("click", (e) => {
+  e.preventDefault();
 
-feelinglucky.addEventListener('click', (e) => {
-    e.preventDefault()
-    
-    
-    if (input.value === 'dogs' || 'pasta') {
+  if (input.value === "dogs" || "pasta") {
     fetch(`http://localhost:3000/${input.value}/random`)
-        .then(resp => resp.text())
-        .then(dog => {
-        searchresult.textContent = dog;
-        })
-    } 
-    else {
-        noResult();
-    }
+      .then((resp) => resp.text())
+      .then((data) => {
+        searchresult.textContent = data;
+      });
+  } else {
+    noResult();
+  }
+});
 
-})
+function changeSite() {
+  location.href = "./search.html";
+}
+
+// module.exports = "dogInfo";
+// let dogInfo = searchresult.textContent;
+//         dogInfo = dog;
+//         changeSite();
+//         return dogInfo;
